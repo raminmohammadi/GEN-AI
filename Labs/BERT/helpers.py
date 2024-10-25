62439020bb1aa61f2dbb7734e624944a8c19ce83
Aditya Digala <adityadigala@Adityas-Laptop.local>
2024-10-25 19:07:02 -0400

    Added BERT Walkthrough

---
+++ Labs/BERT/helpers.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+def plot_metrics(history):
+    # Plot training & validation accuracy values
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'], label='Train Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.title('Model Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(loc='upper left')
+
+    # Plot training & validation loss values
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'], label='Train Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Model Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(loc='upper left')
+
+    plt.tight_layout()
+    plt.show()
